64e19bad4acc3d7f15ff4b64fdc1119ceb1b160e
Perion <nsehnoun@student.42.fr>
2019-11-16 14:22:28 +0100

    Changed text to check git impl. in WS Reformatted file and corrected syntax

--- prostagma-app/src/app/services/auth.service.ts
+++ prostagma-app/src/app/services/auth.service.ts
@@ -1,123 +1,131 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import * as auth0 from 'auth0-js';
-import { Router } from '@angular/router';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { throwError, Observable } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
-import { FormGroup } from '@angular/forms';
-import { environment } from '../../environments/environment';
+import {Router} from '@angular/router';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {environment} from '../../environments/environment';
 
 (window as any).global = window;
+
 @Injectable()
 export class AuthService {
-    auth0 = new auth0.WebAuth({
-        clientID: 'uF6bx7ipLoeTyS4jg1GFiMEZjAA4nVcA',
-        domain: environment.auth.domain,
-        responseType: 'token',
-        redirectUri: environment.auth.auth0RedirectUri,
-        audience: 'http://localhost:3001',
-        scope: 'openid profile email'
+  auth0 = new auth0.WebAuth({
+    clientID: 'uF6bx7ipLoeTyS4jg1GFiMEZjAA4nVcA',
+    domain: environment.auth.domain,
+    responseType: 'token',
+    redirectUri: environment.auth.auth0RedirectUri,
+    audience: 'http://localhost:3001',
+    scope: 'openid profile email'
+  });
+  expiresAt: number;
+  userProfile: any;
+  accessToken: string;
+  authenticated: boolean;
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.getAccessToken();
+  }
+
+  apiUrl = 'http://localhost:3001';
+  prostagmaApiUrl = `${this.apiUrl}/api/prostagmaApi`;
+
+  // private handleError(err: HttpErrorResponse | any) {
+  //   console.error('An error has occurred', err);
+  //   return throwError(err.message || err);
+  // }
+
+  private _setSession(authResult, profile) {
+    this.expiresAt = authResult.expiresIn * 1000 + Date.now();
+    this.accessToken = authResult.accessToken;
+    this.userProfile = profile;
+    this.authenticated = true;
+  }
+
+  getUserInfos(authResult) {
+    this.auth0.client.userInfo(authResult.accessToken, (err, profile) => {
+      if (profile) {
+        console.log(profile);
+        this._setSession(authResult, profile);
+      }
     });
-    expiresAt: Number;
-    userProfile: any;
-    accessToken: string;
-    authenticated: boolean;
+  }
 
-    constructor(private http: HttpClient, private router: Router) {
-        this.getAccessToken();
-    }
-    api_url = "http://localhost:3001";
-    prostagma_api_url = `${this.api_url}/api/prostagmaApi`;
+  getAccessToken() {
+    this.auth0.checkSession({}, (err, authResult) => {
+      if (authResult && authResult.accessToken) {
+        this.getUserInfos(authResult);
+      }
+    });
+  }
 
-    private handleError(err: HttpErrorResponse | any) {
-        console.error('An error occurred', err);
-        return throwError(err.message || err);
-    }
+  handleLoginCallback() {
+    console.log('test');
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken) {
+        window.location.hash = '';
+        this.getUserInfos(authResult);
+      } else if (err) {
+        console.error(`Error ${err.error}}`);
+      }
+      this.router.navigate(['/']).then((callback) => {
+        console.log('Navigated to : ' + callback);
+      });
+    });
+  }
 
-    private _setSession(authResult, profile) {
-        this.expiresAt = authResult.expiresIn * 1000 + Date.now();
-        this.accessToken = authResult.accessToken;
-        this.userProfile = profile;
-        this.authenticated = true;
-    }
+  // login() {
+  //   console.log(this.auth0);
+  //   this.auth0.authorize();
+  // }
 
-    getUserInfos(authResult) {
-        this.auth0.client.userInfo(authResult.accessToken, (err, profile) => {
-            if (profile) {
-                console.log(profile)
-                this._setSession(authResult, profile);
-            }
-        })
-    }
+  logout() {
+    this.auth0.logout({
+      returnTo: environment.auth.auth0ReturnTo,
+      clientID: environment.auth.clientID
+    });
+  }
 
-    getAccessToken() {
-        this.auth0.checkSession({}, (err, authResult) => {
-            if (authResult && authResult.accessToken) {
-                this.getUserInfos(authResult);
-            }
-        })
-    }
+  get isLoggedIn(): boolean {
+    return Date.now() < this.expiresAt && this.authenticated;
+  }
 
-    handleLoginCallback(){
-        console.log("test");
-        this.auth0.parseHash((err, authResult) => {
-            if (authResult && authResult.accessToken) {
-                window.location.hash = '';
-                this.getUserInfos(authResult);
-            } else if (err) {
-                console.error(`Error ${err.error}}`)
-            }
-            this.router.navigate(['/']);
-        });
-    }
+  team() {
+    return (this.http.get(this.prostagmaApiUrl + '/teams', {
+        headers: new HttpHeaders().set('Authorization', `Bearer ${this.accessToken}`)
+      }).pipe(map(res => {
+        console.log(res);
+      }))
+    );
+  }
 
-    login() {
-        console.log(this.auth0);
-        this.auth0.authorize();
-    }
+  connect(): Observable<any> {
+    console.log('Auth service landed');
+    console.log(this.prostagmaApiUrl);
+    return (this.http.get(this.prostagmaApiUrl + '/connect').pipe(map(res => {
+        console.log(res);
+      })
+    ));
+  }
 
-    logout() {
-        this.auth0.logout({
-            returnTo: environment.auth.auth0ReturnTo,
-            clientID: environment.auth.clientID
-        });
-    }
+  saveUser(formGroup): Observable<any> {
+    console.log(formGroup);
+    return (this.http.post(this.prostagmaApiUrl + '/db/saveUser', formGroup).pipe(map(res => {
+      console.log(res);
+    })));
+  }
 
-    get isLoggedIn(): boolean {
-        return Date.now() < this.expiresAt && this.authenticated;
-    }
-    team() {
-        return (this.http.get(this.prostagma_api_url + '/teams', {
-            headers: new HttpHeaders().set('Authorization', `Bearer ${this.accessToken}`)
-        }).pipe(map(res => {
-            console.log(res);
-        }))
-        )
-    }
-    connect(): Observable<any> {
-        console.log("Auth service landed");
-        console.log(this.prostagma_api_url);
-        return (this.http.get(this.prostagma_api_url + '/connect').pipe(map(res => {
-            console.log(res);
-        })
-        ))
-    }
-    saveUser(formGroup): Observable<any> {
-        console.log(formGroup);
-        return (this.http.post(this.prostagma_api_url + '/db/saveUser', formGroup).pipe(map(res => {
-            console.log(res);
-        })));
-    }
-    subscribe(formGroup): Observable<any> {
-        console.log(formGroup);
-        return (this.http.post(this.prostagma_api_url + '/subscribe', formGroup).pipe(map(res => {
-            console.log(res);
-        })))
-    }
-    log(formGroup): Observable<any> {
-        console.log(formGroup);
-        return (this.http.get(this.prostagma_api_url + '/db/connect').pipe(map(res => {
-            console.log(res);
-        })))
-    }
+  // subscribe(formGroup): Observable<any> {
+  //   console.log(formGroup);
+  //   return (this.http.post(this.prostagmaApiUrl + '/subscribe', formGroup).pipe(map(res => {
+  //     console.log(res);
+  //   })));
+  // }
+
+  log(formGroup): Observable<any> {
+    console.log(formGroup);
+    return (this.http.get(this.prostagmaApiUrl + '/db/connect').pipe(map(res => {
+      console.log(res);
+    })));
+  }
 }
